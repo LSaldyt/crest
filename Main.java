be3bb3f33ecf3bc8c8445b54795554a7cf59b7c2
LSaldyt <lucassaldyt@gmail.com>
2017-09-10 14:04:40 -0600

    Adds better abstraction through Optimizer class.

--- Main.java
+++ Main.java
@@ -4,19 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Reader reader = new Reader();
-        RankedSet rankedSet = reader.read_set("cars.csv");
-        Criteria criteria = reader.read_criteria("criteria.csv");
-
-        /*
-        Criteria criteria = new Criteria();
-        criteria.add("price", new Metric(10.0, false));
-        criteria.add("efficiency", new Metric(1.0, true));
-        criteria.add("mileage", new Metric(1.0, false));
-        */
-
-        criteria.sort(rankedSet);
-        System.out.println(criteria);
-        System.out.println(rankedSet);
+        Optimizer optimizer = new Optimizer();
+        optimizer.find_optimal("cars");
+        optimizer.find_optimal("cars", "price.csv");
     }
 }
---
+++ Optimizer.java
@@ -0,0 +1,26 @@
+package optimal;
+
+public class Optimizer
+{
+    public Optimizer()
+    {
+
+    }
+
+    public void find_optimal(String directory)
+    {
+        find_optimal(directory, "criteria.csv");
+    }
+
+    public void find_optimal(String directory, String criteriaFile)
+    {
+        Reader reader = new Reader();
+        RankedSet rankedSet = reader.read_set("data/" + directory + "/items.csv");
+        Criteria criteria = reader.read_criteria("data/" + directory + "/" + criteriaFile);
+
+        criteria.sort(rankedSet);
+        System.out.println(criteria);
+        System.out.println(rankedSet);
+
+    }
+}
